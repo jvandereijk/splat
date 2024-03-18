1f60ad80f69752ce0071b3b388f858949569b3e8
Jack van der Eijk <info@jackvandereijk.nl>
2024-03-18 08:37:13 +0100

    Minimal test project

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+/** @type {import('vite').Plugin} */
+const viteServerConfig = {
+	name: "cors-header-middleware",
+	configureServer(server) {
+		server.middlewares.use((req, res, next) => {
+			res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+			res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+			next();
+		});
+	},
+};
+
+/** @type {import('@vitejs/plugin-vue').Config} */
+const config = {
+	plugins: [viteServerConfig],
+
+	// other configurations...
+};
+
+export default config;
